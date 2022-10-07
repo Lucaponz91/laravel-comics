8419cb99bb1a68f232fd83064d460ff444cde1c1
Lucaponz91 <lucapo1991@msn.com>
2022-10-07 20:39:32 +0200

    add header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,23 @@
+
+<header>
+        <div class="header_container">
+        <img src="{{asset('img/dc-logo.png')}}" alt="logo">
+        <ul>
+            <li><a href="">CHARACTERS</a></li>
+            <li><a href="">COMICS</a> </li>
+            <li><a href="">MOVIES</a></li>
+            <li><a href="">TV</a></li>
+            <li><a href="">GAMES</a></li>
+            <li><a href="">COLLECTIBLES</a></li>
+            <li><a href="">VIDEOS</a></li>
+            <li><a href="">FANS</a></li>
+            <li><a href="">NEWS</a></li>
+            <li><a href="">SHOP</a></li>
+            
+        </ul>
+
+    </div>
+    
+</header>
+
+
