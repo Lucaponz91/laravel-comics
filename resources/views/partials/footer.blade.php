71309c6d6bbb3a0e8d101995577b5886c4975e52
Lucaponz91 <lucapo1991@msn.com>
2022-10-08 09:42:10 +0200

    add footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,75 @@
-FOOTER
+<div class="footer_container">
+    <div class="footer_store_buy">
+        <ul>
+            <li><img src="{{asset('img/buy-comics-digital-comics.png')}}" alt=""><span>COMICS</span> </li>
+            <li><img src="{{asset('img/buy-comics-merchandise.png')}}" alt=""><span>DC MERCHANDISE</span></li>
+            <li><img src="{{asset('img/buy-comics-subscriptions.png')}}" alt=""><span>SUBSCRIPTION</span></li>
+            <li><img src="{{asset('img/buy-comics-shop-locator.png')}}" alt=""><span>COMIC SHOP LOCATOR</span></li>
+            <li><img src="{{asset('img/buy-dc-power-visa.svg')}}" alt=""><span>DC POWER VISA</span></li>
+        </ul>
+
+    </div>
+    <div class="footer_dc_links">
+        <div class="footer_links_left">
+            <div class="links_left">
+                <ul><span>DC COMICS</span>
+                    <li><a href="">Characters</a> </li>
+                    <li><a href="">Comics</a> </li>
+                    <li><a href="">Movies</a> </li>
+                    <li><a href="">TV</a> </li>
+                    <li><a href="">Games</a> </li>
+                    <li><a href="">Videos</a> </li>
+                    <li><a href="">News</a> </li>
+                </ul>
+                <ul><span>SHOP</span>
+                    <li><a href="">Shop DC</a> </li>
+                    <li><a href="">Shop DC Collectibles</a> </li>
+                </ul>
+
+            </div>
+            <div class="links_middle">
+                <ul><span>DC</span>
+                    <li><a href=""> Terms Of Use</a></li>
+                    <li><a href=""> Privacy policy (New)</a></li>
+                    <li><a href=""> Ad Choices</a></li>
+                    <li><a href=""> Advertising</a></li>
+                    <li><a href=""> Jobs</a></li>
+                    <li><a href=""> Subscriptions</a></li>
+                    <li><a href=""> Talent Workshops</a></li>
+                    <li><a href=""> CPSC Certificates</a></li>
+                    <li><a href=""> Ratings</a></li>
+                    <li><a href=""> Shop Help</a></li>
+                    <li><a href=""> Contact Us</a></li>
+
+                </ul>
+
+            </div>
+            <div class="links_right">
+                <ul><span>SITES</span>
+                    <li><a href=""> DC</a></li>
+                    <li><a href=""> MAD Magazine</a></li>
+                    <li><a href=""> DC Kids</a></li>
+                    <li><a href=""> DC Universe</a></li>
+                    <li><a href=""> DC Power Visa</a></li>
+                </ul>
+
+            </div>
+        </div>
+        <div class="footer_logo_right">
+            
+        </div>
+
+    </div>
+    <div class="signup_n_socials">
+        <a href=""><span>SIGN-UP NOW!</span></a>
+        <div class="socials">
+            <span>FOLLOW US</span>
+            <a href=""><img src="{{asset('img/footer-facebook.png')}}" alt=""></a>
+            <a href=""><img src="{{asset('img/footer-twitter.png')}}" alt=""></a>
+            <a href=""><img src="{{asset('img/footer-youtube.png')}}" alt=""></a>
+            <a href=""><img src="{{asset('img/footer-pinterest.png')}}" alt=""></a>
+            <a href=""><img src="{{asset('img/footer-periscope.png')}}" alt=""></a>
+        </div>
+
+    </div>
+</div>
