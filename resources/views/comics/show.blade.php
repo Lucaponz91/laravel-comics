88f42e17228e5f96c66c7a5cbf634200e8916e35
Lucaponz91 <lucapo1991@msn.com>
2022-10-11 10:06:06 +0200

    fix

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -2,12 +2,14 @@
 @section('metaTitle',$comic['title'])
 
 @section('content')
-<div>
-    <div>
-        <h1>{{$comic['title']}}</h1>
-        <h4>{{$comic['price']}}</h4>
-        <p>{{$comic['description']}} </p>
+<div class="show_container">
+    <div class="show_main">
+        <div>
+            <h1>{{$comic['title']}}</h1>
+            <h4>{{$comic['price']}}</h4>
+            <p>{{$comic['description']}} </p>
+        </div>
+        <img src="{{asset('img/adv.jpg')}}" alt="">
     </div>
-    <img src="" alt="">
 </div>
 @endsection
